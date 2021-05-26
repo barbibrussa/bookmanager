e77bc75c9551a33e4de02ac12e97fb889d5d275a
barbibrussa <barbibrussa@gmail.com>
2021-05-26 13:22:56 -0300

    Add database initialization and migrations

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/barbibrussa/bookmanager/pkg/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+)
 
+func main() {
+	db, err := gorm.Open(sqlite.Open("book_manager.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error while connecting to the database: ", err)
+	}
+
+	err = db.AutoMigrate(&models.Book{}, &models.Checkout{})
+	if err != nil {
+		log.Fatal("Error while auto-migrating models: ", err)
+	}
 }
