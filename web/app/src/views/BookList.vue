bd451889ab8b1ac51d76617751b0a55a5c29ed78
barbibrussa <barbibrussa@gmail.com>
2021-06-14 17:56:38 -0300

    Implement page to list books

--- web/app/src/views/BookList.vue
+++ web/app/src/views/BookList.vue
@@ -1,12 +1,50 @@
 <template>
   <div>
-
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left">
+            ID
+          </th>
+          <th class="text-left">
+            Title
+          </th>
+          <th class="text-left">
+            Author
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+          v-for="item in list"
+          :key="item.ID"
+        >
+          <td>{{ item.ID }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.author }}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BookList',
+  beforeCreate() {
+    fetch('http://localhost:3030/books').then(
+      async (response) => {
+        this.list = await response.json();
+      },
+    );
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
 };
 </script>
 
