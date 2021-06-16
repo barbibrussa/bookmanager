cc0730cdace1ce09f8765c978f7694cd29e08a93
barbibrussa <barbibrussa@gmail.com>
2021-06-16 19:44:03 -0300

    Fix borrow book and delete book methods

--- web/app/src/views/BookList.vue
+++ web/app/src/views/BookList.vue
@@ -27,143 +27,134 @@
           <td>{{ item.title }}</td>
           <td>{{ item.author }}</td>
           <td>
-            <v-icon title="Informacion"
+            <v-icon
+              title="Pedir prestado"
+              @click="selectBook(item.ID); showBorrowDialog();"
               small
               class="mr-2"
-              @click="showInfo(item.ID)"
             >
-              mdi-alert-circle-outline
+              mdi-pencil
             </v-icon>
-            <v-dialog
-              v-model="dialog"
-              persistent
-              max-width="600px"
+            <v-icon
+              title="Borrar libro"
+              @click="selectBook(item.ID); showDeleteDialog()"
             >
-              <template v-slot:activator="{ on, attrs }">
-                <v-icon title="Pedir prestado" @click="selectBook(item.ID)"
-                  small
-                  class="mr-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  mdi-pencil
-                </v-icon>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="text-h5">Prestar libro</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field v-model="firstName"
-                          label="Nombre*"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field v-model="lastName"
-                          label="Apellido*"
-                          persistent-hint
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field v-model="dni"
-                          label="DNI*"
-                          persistent-hint
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field v-model="phoneNumber"
-                          label="Número de teléfono++*"
-                          persistent-hint
-                          required
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                  <small>*campos obligatorios</small>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="dialog = false"
-                  >
-                    Cerrar
-                  </v-btn>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="borrowBook()"
-                  >
-                    Confirmar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <v-dialog
-              v-model="dialog"
-              persistent
-              max-width="290"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-icon title="Borrar libro" @click="selectBook(item.ID)"
-                  small v-bind="attrs"
-                        v-on="on"
-                >
-                mdi-delete
-                </v-icon>
-              </template>
-              <v-card>
-                <v-card-title class="text-h5">
-                  ¿Desea borrar el libro?
-                </v-card-title>
-                <v-card-text>Confirme para eliminarlo o aprete cancelar</v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="green darken-1"
-                    text
-                    @click="dialog = false"
-                  >
-                    Cancelar
-                  </v-btn>
-                  <v-btn
-                    color="green darken-1"
-                    text
-                    @click="deleteBook()"
-                  >
-                    Eliminar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+              mdi-delete
+            </v-icon>
             </td>
           </tr>
           </tbody>
         </template>
       </v-simple-table>
+      <div>
+        <v-dialog
+          v-model="borrowDialog"
+          persistent
+          max-width="600px"
+        >
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Prestar libro</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field v-model="firstName"
+                                  label="Nombre*"
+                                  required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field v-model="lastName"
+                                  label="Apellido*"
+                                  persistent-hint
+                                  required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field v-model="dni"
+                                  label="DNI*"
+                                  persistent-hint
+                                  required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field v-model="phoneNumber"
+                                  label="Número de teléfono++*"
+                                  persistent-hint
+                                  required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*campos obligatorios</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="borrowDialog = false"
+              >
+                Cerrar
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="borrowBook()"
+              >
+                Confirmar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog
+          v-model="removeDialog"
+          persistent
+          max-width="290"
+        >
+          <v-card>
+            <v-card-title class="text-h5">
+              ¿Desea borrar el libro?
+            </v-card-title>
+            <v-card-text>Confirme para eliminarlo o aprete cancelar</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="green darken-1"
+                text
+                @click="removeDialog = false"
+              >
+                Cancelar
+              </v-btn>
+              <v-btn
+                color="green darken-1"
+                text
+                @click="deleteBook()"
+              >
+                Eliminar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
     </div>
 </template>
 
@@ -183,7 +174,8 @@ export default {
     return {
       list: [],
       book: {},
-      dialog: false,
+      borrowDialog: false,
+      removeDialog: false,
       firstName: '',
       lastName: '',
       dni: '',
@@ -191,16 +183,15 @@ export default {
     };
   },
   methods: {
-    showInfo(id) {
-      client.get(`/books/${id}`).then((res) => console.log(res.data));
-    },
     deleteBook() {
       client.delete(`/books/${this.selected}`)
         .then((res) => console.log(res.data))
         .catch((err) => console.error(err));
 
-      const i = this.list.findIndex((item) => item.id === this.selected);
+      const i = this.list.findIndex((item) => item.ID === this.selected);
       this.list.splice(i, 1);
+
+      this.removeDialog = false;
     },
     selectBook(id) {
       this.selected = id;
@@ -212,7 +203,13 @@ export default {
         dni: this.dni,
         phone_number: this.phoneNumber,
       }).then((res) => console.log(res));
-      this.dialog = false;
+      this.borrowDialog = false;
+    },
+    showBorrowDialog() {
+      this.borrowDialog = true;
+    },
+    showDeleteDialog() {
+      this.removeDialog = true;
     },
   },
 };
