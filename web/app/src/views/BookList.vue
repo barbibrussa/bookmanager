35bf885d3253d3ce33be23c8167e08d39a67866d
barbibrussa <barbibrussa@gmail.com>
2021-06-16 16:13:49 -0300

    Add borrow book and delete book methods

--- web/app/src/views/BookList.vue
+++ web/app/src/views/BookList.vue
@@ -8,13 +8,13 @@
             ID
           </th>
           <th class="text-left">
-            Title
+            Título
           </th>
           <th class="text-left">
-            Author
+            Autor
           </th>
           <th class="text-left">
-            Actions
+            Acciones
           </th>
         </tr>
         </thead>
@@ -27,32 +27,144 @@
           <td>{{ item.title }}</td>
           <td>{{ item.author }}</td>
           <td>
-            <v-icon
+            <v-icon title="Informacion"
               small
               class="mr-2"
               @click="showInfo(item.ID)"
             >
               mdi-alert-circle-outline
             </v-icon>
-            <v-icon
-              small
-              class="mr-2"
-              @click="borrowBook(item.ID)"
+            <v-dialog
+              v-model="dialog"
+              persistent
+              max-width="600px"
             >
-              mdi-pencil
-            </v-icon>
-            <v-icon
-              small
-              @click="deleteBook(item.ID)"
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon title="Pedir prestado" @click="selectBook(item.ID)"
+                  small
+                  class="mr-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  mdi-pencil
+                </v-icon>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">Prestar libro</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                        <v-text-field v-model="firstName"
+                          label="Nombre*"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                        <v-text-field v-model="lastName"
+                          label="Apellido*"
+                          persistent-hint
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                        <v-text-field v-model="dni"
+                          label="DNI*"
+                          persistent-hint
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                        <v-text-field v-model="phoneNumber"
+                          label="Número de teléfono++*"
+                          persistent-hint
+                          required
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                  <small>*campos obligatorios</small>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="dialog = false"
+                  >
+                    Cerrar
+                  </v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="borrowBook()"
+                  >
+                    Confirmar
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog
+              v-model="dialog"
+              persistent
+              max-width="290"
             >
-              mdi-delete
-            </v-icon>
-          </td>
-        </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-  </div>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon title="Borrar libro" @click="selectBook(item.ID)"
+                  small v-bind="attrs"
+                        v-on="on"
+                >
+                mdi-delete
+                </v-icon>
+              </template>
+              <v-card>
+                <v-card-title class="text-h5">
+                  ¿Desea borrar el libro?
+                </v-card-title>
+                <v-card-text>Confirme para eliminarlo o aprete cancelar</v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="green darken-1"
+                    text
+                    @click="dialog = false"
+                  >
+                    Cancelar
+                  </v-btn>
+                  <v-btn
+                    color="green darken-1"
+                    text
+                    @click="deleteBook()"
+                  >
+                    Eliminar
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            </td>
+          </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </div>
 </template>
 
 <script>
@@ -71,27 +183,36 @@ export default {
     return {
       list: [],
       book: {},
+      dialog: false,
+      firstName: '',
+      lastName: '',
+      dni: '',
+      phoneNumber: '',
     };
   },
   methods: {
     showInfo(id) {
       client.get(`/books/${id}`).then((res) => console.log(res.data));
     },
-    deleteBook(id) {
-      client.delete(`/books/${id}`)
+    deleteBook() {
+      client.delete(`/books/${this.selected}`)
         .then((res) => console.log(res.data))
         .catch((err) => console.error(err));
 
-      const i = this.list.findIndex((item) => item.id === id);
+      const i = this.list.findIndex((item) => item.id === this.selected);
       this.list.splice(i, 1);
     },
-    borrowBook(id) {
-      client.post(`/books/${id}/borrow`, {
-        first_name: prompt('Ingrese el nombre'),
-        last_name: prompt('Ingrese el apellido'),
-        dni: prompt('Ingrese el DNI'),
-        phone_number: prompt('Ingrese el numero de telefono'),
+    selectBook(id) {
+      this.selected = id;
+    },
+    borrowBook() {
+      client.post(`/books/${this.selected}/borrow`, {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        dni: this.dni,
+        phone_number: this.phoneNumber,
       }).then((res) => console.log(res));
+      this.dialog = false;
     },
   },
 };
