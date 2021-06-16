eb280c83e813782d87d16c69552a566be26e2053
barbibrussa <barbibrussa@gmail.com>
2021-06-16 16:12:33 -0300

    Add toolbar title

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -6,34 +6,22 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-toolbar-title>Gestor de libros</v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        to="/books"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        Libros
+      </v-btn>
+      <v-btn
+        to="/checkouts"
+        text
+      >
+        Préstamos
       </v-btn>
     </v-app-bar>
 
