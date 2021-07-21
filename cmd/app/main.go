a7c11e76e6ad7304d093407634083f65fa0f7c33
barbibrussa <barbibrussa@gmail.com>
2021-07-21 18:34:17 -0300

    Validate user and password credentials for basic authentication

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -17,11 +17,13 @@ func main() {
 	db, err := gorm.Open(sqlite.Open("book_manager.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error while connecting to the database: ", err)
+		return
 	}
 
 	err = db.AutoMigrate(&models.Book{}, &models.Checkout{}, &models.Review{})
 	if err != nil {
 		log.Fatal("Error while auto-migrating models: ", err)
+		return
 	}
 
 	r := chi.NewRouter()
@@ -40,6 +42,11 @@ func main() {
 
 	user := os.Getenv("APP_USER")
 	password := os.Getenv("APP_PASSWORD")
+
+	if len(user) == 0 || len(password) == 0 {
+		log.Fatalf("Error: empty user and password credentials")
+		return
+	}
 
 	r.Use(middleware.BasicAuth("librarians", map[string]string{
 		user: password,
@@ -60,5 +67,6 @@ func main() {
 	err = http.ListenAndServe(":3030", r)
 	if err != nil {
 		log.Fatal("Error serving HTPP on port :3030 ", err)
+		return
 	}
 }
